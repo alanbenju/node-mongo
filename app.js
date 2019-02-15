6993b9f5ee6aec6e49b773fa0bd142a02066f243
alanbenju <alanbenju>
2019-02-15 01:08:09 -0300

    mongo port

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const url = process.env.MONGO_URL || 'mongodb://localhost:27017'
 const app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+console.log("Mongo url:",url)
 
 // Connect to MongoDB
 mongoose
   .connect(
-    'mongodb://mongo:27018/node-server',
+    url,
     { useNewUrlParser: true }
   )
   .then(() => console.log('MongoDB Connected'))
